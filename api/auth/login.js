8573a75bf227411c0da80223a1067ad47e751220
Dave8011 <saurabhdave8@gmail.com>
2025-06-07 13:45:42 +0530

    Update login.js

--- api/auth/login.js
+++ api/auth/login.js
@@ -1,13 +1,11 @@
-export default async function handler(req, res) {
+// api/auth/login.js
+
+export default function handler(req, res) {
   const { username, password } = req.query;
 
-  if (username === process.env.ADMIN_USER && password === process.env.ADMIN_PASS) {
-    return res.status(200).json({ admin: true });
+  if (username === "Dave8011" && password === "Qwerty@1.1") {
+    res.status(200).json({ admin: true });
+  } else {
+    res.status(401).json({ admin: false });
   }
-
-  const client_id = process.env.GITHUB_CLIENT_ID;
-  const redirect_uri = `${req.headers.origin}/api/auth/callback`;
-
-  const githubAuthUrl = `https://github.com/login/oauth/authorize?client_id=${client_id}&redirect_uri=${redirect_uri}&scope=repo`;
-  res.redirect(githubAuthUrl);
 }
